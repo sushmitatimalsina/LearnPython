9dd8d10be70332d813ad18c0dc8e1d23bb62fc71
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-09-11 22:52:47 +0545

    feat: Practice Python: hello world, arithmetic, variables, and string formatting

--- hello.py
+++ hello.py
@@ -1,2 +1,63 @@
 print ("hello Sushmita!")
 print("Python is working!")
+
+print(123)
+print(1+2)
+
+print('hello')
+print("a"+"b")
+print("1"+"a")
+
+# addition
+print(1+2)
+
+# subtraction
+print(1-2)
+
+# multiplication
+print(5*5)
+
+# division
+print(8/2)
+
+# exponent -(we use **  to denote exponents)
+print(2**3)
+
+# modulus (remainder)-%
+print(6%2)
+print(3%2)
+
+print(5-10+5)
+print(-100+20+5)
+
+name="sushmita"
+print(name)
+print('Timalsina')
+
+# num1="12"
+# num2=3
+# # print(num1/num2)
+
+print(type("ram"))
+print(type(name))
+
+# print(a)
+
+name = "Sushmita Timalsina"
+age = 32
+percentage = 80.55
+address = "Kathmandu"
+
+print(name, age, percentage, address)
+print("My name is ", name)
+print("My name is ", name, "I am ", age, " years old.", percentage, address)
+
+print(f"My name is {name}. I am {age} years old. I live in {address}.")
+
+type(name)
+
+age = 23
+print("Age 1", age)
+print("Age 2", age)
+age = 34
+print("Age 3",age)
