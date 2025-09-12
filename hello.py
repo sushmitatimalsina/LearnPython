7e699383b6697e31d2d257ba4c987df3556b7c0c
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-09-12 22:39:43 +0545

    completed simple arthematic operations logical,

--- hello.py
+++ hello.py
@@ -61,3 +61,73 @@ print("Age 1", age)
 print("Age 2", age)
 age = 34
 print("Age 3",age)
+
+
+a = 10
+b = 20
+# Floor Division:
+#  division that rounds the result down to the nearest whole number, effectively discarding any fractional part
+print(a//b)
+
+x=15
+x+=5
+print(x)
+
+x/=5
+print(x)
+
+x%=5
+print(x)
+
+m = 10.0
+n = 20
+print("Equal:", m == n)
+print("Not Equal:", m != n)
+print("Greater than:", m > n)
+print("Less than:", m < n)
+
+s = "hello"
+
+print("Equal:", m == s)
+
+print(type(m))
+h = int(a) ## explicit type conversion
+print(type(h))
+
+age = 20
+has_id = True
+print("Can vote:", age >= 18 and has_id)
+
+# name = input("Enter your name:")
+# age = input("Enter your age:")
+# print(name, age)
+# print(f"{name} was born in {2025-int(age)}.")
+
+
+l, p, d = "hi" ,11, 5.6
+print(l, p, d)
+
+fruits = ["apple", "banana", "cherry"]
+e, t, r = fruits
+print(e, t, r)
+
+# x = "Python "
+# y = "is "
+# z = "awesome"
+# print(x + y + z)
+
+# j = "awesome"
+# def myfunc():
+#     print("python is " + j)
+#     myfunc()
+
+address = "Kathmandu"
+print(len(address))
+
+txt = "The best things in life are free!"
+# print("free" in txt)
+
+if "khusi" in txt:
+  print("Yes, 'free' is present.")
+
+  print(txt[7:11])
