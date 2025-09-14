67a22dad64821041730f19ab1070ceb506ce1c3a
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-09-14 10:56:09 +0545

    completed condition +loop

--- condition.py
+++ condition.py
@@ -7,4 +7,24 @@ if b > a:
 elif a == b:
     print("a and b are equal")
 
-    
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+
+for m in range(10):
+   print(m)
+else:
+   print("finally finish")
+
+for n in range(2, 30, 3):
+   if n == 8: continue
+   print(n)
+else:
+   print("finally finished")
+
+   adj = ["red", "big", "tasty"]
+   for j in adj:
+      for k in fruits:
+         print(j, k)
