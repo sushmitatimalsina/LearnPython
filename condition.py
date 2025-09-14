4d09dade4591d4be9c62336258ebb2297eb1b58b
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-09-14 10:32:35 +0545

    learned conditions in pythons

---
+++ condition.py
@@ -0,0 +1,10 @@
+a = 33
+b = 33
+if b > a:
+    print("b is grater than a")
+
+    # elif- if previous condition werenot true then try this conditions
+elif a == b:
+    print("a and b are equal")
+
+    
