0723ccfb1c490d10f80eda682e6fc8dd57c2fd92
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-09-15 11:42:24 +0545

    feat: add Python list and NumPy array examples with basic operations

---
+++ list.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+fruits = ["apple", "banana", "mango"]
+print(fruits[0])
+print(fruits[2])
+fruits.append("orange")
+print(fruits)
+
+nums = np.array([1, 2, 3, 4, 5])
+print(nums * 2) 
+
+number = [1,2,3,4,5]
+print(number * 2)
+
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+print(a + b)
