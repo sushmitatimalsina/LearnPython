c580ccb880c8e856ae6ce87d59063d2e017b2f70
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-09-15 11:04:30 +0545

    feat: add and fix Python function examples

---
+++ function.py
@@ -0,0 +1,38 @@
+# def my_function():
+
+#  print("hello ")
+
+# my_function()
+
+def details(fname):
+    print(fname + " Timalsina")   
+
+details("Sushmita")
+details("Sushil")
+details("Saraswoti")
+details("Govinda")
+
+def student(name,sirname):
+    print(name + " " + sirname)
+
+student("Asmita", "Timalsina")    
+
+def fruits(*name):
+    print("My fav fruits is " + name[2])
+fruits("apple", "banana", "Mango")
+
+def child(child3, child2, child1):
+    print("The younger child is " + child3)
+
+child(child1 = "Emil", child2 = "Tobias", child3 = "Linus")    
+
+def hi(food):
+    for x in food:
+        print(x)
+game = ("a", "b", "c")
+hi(game)
+
+def add(x):
+    return 5 + x
+# print(add(5))
+print("Sum is " + str(add(10)))
