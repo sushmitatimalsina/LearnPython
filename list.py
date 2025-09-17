3c6b9e561704005b172d28f2693defa44ec3e9a9
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-09-17 21:48:47 +0545

    learned array in python

--- list.py
+++ list.py
@@ -1,4 +1,5 @@
 import numpy as np
+import array as arr
 
 
 fruits = ["apple", "banana", "mango"]
@@ -16,3 +17,14 @@ print(number * 2)
 a = np.array([1, 2, 3])
 b = np.array([4, 5, 6])
 print(a + b)
+
+arry = arr.array('d',[1.1, 1.2, 1.3,])
+print(arry[1])
+arry.append(1.4)
+print(arry)
+for x in arry:
+    print(x)
+print("=====")
+for y in arry[1:3]:
+    print(y)
+    
