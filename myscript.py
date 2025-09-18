33ab4c3df31d445bfecee7b2e35798459a0bcd97
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-09-18 23:05:50 +0545

    learn input values

---
+++ myscript.py
@@ -0,0 +1,8 @@
+fname = input("Enter your name: ")
+print("Hello,", fname)
+
+val = input("Enter your value: ")
+print(val)
+
+num = int(input("Enter a number: "))
+print(num, "is of type", type(num))
