338db2d72fa2b264633182660ae3c17b97db1b38
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-09-25 16:29:36 +0545

    learn basic concept about boolean in python

---
+++ boolean1.py
@@ -0,0 +1,21 @@
+print(10 > 9)
+
+print(10 == 9)
+
+print(10 < 9)
+# false
+bool("abc")
+print(bool(0))
+# true 
+print(bool("abc")) 
+
+
+# true
+def myFunction() :
+    return True
+print(myFunction())
+
+x = 200
+print(isinstance(x, int))
+print(isinstance(x, float))
+
