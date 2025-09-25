adf420a9f5f0820515b0a3360a50763d88d678cc
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-09-25 14:04:47 +0545

    learn selected sort

--- DSA/selected_sort.py
+++ DSA/selected_sort.py
@@ -1,14 +1,14 @@
-# arr = [7, 11, 9, 12, 3]
-# n = len(arr)
+arr = [7, 11, 9, 12, 3]
+n = len(arr)
 
-# for i in range(n - 1):
-#     min_index = i
-#     for j in range(i + 1, n):
-#         if arr[j] < arr[min_index]:
-#             min_index = j
-#     arr[i], arr[min_index] = arr[min_index], arr[i]
+for i in range(n - 1):
+    min_index = i
+    for j in range(i + 1, n):
+        if arr[j] < arr[min_index]:
+            min_index = j
+    arr[i], arr[min_index] = arr[min_index], arr[i]
 
-# print("sorted value is ", arr)
+print("sorted value is ", arr)
 
 
 my_array = list(map(int, input("enter your number :") . split() ) )
@@ -24,5 +24,7 @@ for h in range(m-1):
             min = k
     b[h], b[min] = b[min], b[h]
 print(b)          
+
+
 
 
