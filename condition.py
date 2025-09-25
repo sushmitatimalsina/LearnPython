fb1c3295bf685bc74adeae77ed93092c898d951d
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-09-25 17:19:55 +0545

    learn about the logic of finding max,min,reverse,sort in pythons

--- condition.py
+++ condition.py
@@ -1,30 +1,37 @@
-a = 33
-b = 33
-if b > a:
-    print("b is grater than a")
+# a = 33
+# b = 33
+# if b > a:
+#     print("b is grater than a")
 
-    # elif- if previous condition werenot true then try this conditions
-elif a == b:
-    print("a and b are equal")
+#     # elif- if previous condition werenot true then try this conditions
+# elif a == b:
+#     print("a and b are equal")
 
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-  if x == "banana":
-    continue
-  print(x)
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   if x == "banana":
+#     continue
+#   print(x)
 
-for m in range(10):
-   print(m)
-else:
-   print("finally finish")
+# for m in range(10):
+#    print(m)
+# else:
+#    print("finally finish")
 
-for n in range(2, 30, 3):
-   if n == 8: continue
-   print(n)
-else:
-   print("finally finished")
+# for n in range(2, 30, 3):
+#    if n == 8: continue
+#    print(n)
+# else:
+#    print("finally finished")
 
-   adj = ["red", "big", "tasty"]
-   for j in adj:
-      for k in fruits:
-         print(j, k)
+#    adj = ["red", "big", "tasty"]
+#    for j in adj:
+#       for k in fruits:
+#          print(j, k)
+numbers = [12, 45, 7, 23, 89, 5, 33]
+
+max_num = numbers[0]
+for num in numbers:
+    if num > max_num:
+        max_num = num 
+print("Maximum number:", max_num)        
