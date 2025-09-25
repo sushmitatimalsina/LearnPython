14c8c16cc2847d315f7490b5acf1009600e309b1
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-09-25 16:55:45 +0545

    learn  basic concept baout the user inputs

---
+++ user_input.py
@@ -0,0 +1,15 @@
+
+import math
+# multiple inputs 
+name = input("Enter your name:")
+
+print(f"Hello {name}")
+fav1 = input("What is your favorite animal:")
+fav2 = input("What is your favorite color:")
+fav3 = input("What is your favorite number:")
+print(f"Do you want a {fav1} {fav2} with {fav3} legs?")
+
+# input number
+x = input("Enter a number:")
+y = math.sqrt(float(x))
+print(f"The square root of {x} is {y}")
