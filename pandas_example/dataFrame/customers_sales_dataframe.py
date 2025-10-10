f5703cb124b5f7949d01c44d39b239b3529a43ce
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-10-10 22:46:21 +0545

    feat: add sample customer and sales datasets for data cleaning

---
+++ pandas_example/dataFrame/customers_sales_dataframe.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+customers = pd.DataFrame({
+    'customer_id': [101, 102, 103, 104, 105, 106],
+    'customer_name': ['Sita', 'Gita', 'Hari', 'Rita', 'Nabin', None],
+    'city': ['Kathmandu', 'Pokhara', 'Lalitpur', None, 'Biratnagar', 'Pokhara'],
+    'age': [25, 30, None, 45, 28, 35]
+})
+
+
+sales = pd.DataFrame({
+    'order_id': [201, 202, 203, 204, 205, 206],
+    'customer_id': [101, 102, 103, 107, 105, 102],
+    'product': ['Laptop', 'Mobile', 'Tablet', 'Laptop', 'Headphones', 'Laptop'],
+    'amount': [85000, None, 45000, 90000, 7000, 88000],
+    'order_date': ['2024-02-10', '2024-03-12', '2024-03-14', '2024-04-01', '2024-05-15', '2024-06-10']
+})
+
+customers[ 'customer_name'].fillna('Unknown', inplace = True)
+print(customers)
+
+customers['city'].fillna('Not Provided', inplace=True)
+print("After filling city:")
+print(customers)
