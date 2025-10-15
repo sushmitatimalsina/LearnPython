1b166a3faeb98a1601e243eccddc9bf55ba57506
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-10-15 22:24:36 +0545

    learn simple example

--- user_input.py
+++ user_input.py
@@ -1,16 +1,23 @@
-
+import pandas as pd 
 import math
-# multiple inputs 
-name = input("Enter your name:")
+# # multiple inputs 
+# name = input("Enter your name:")
 
-print(f"Hello {name}")
-fav1 = input("What is your favorite animal:")
-fav2 = input("What is your favorite color:")
-fav3 = input("What is your favorite number:")
-print(f"Do you want a {fav1} {fav2} with {fav3} legs?")
+# print(f"Hello {name}")
+# fav1 = input("What is your favorite animal:")
+# fav2 = input("What is your favorite color:")
+# fav3 = input("What is your favorite number:")
+# print(f"Do you want a {fav1} {fav2} with {fav3} legs?")
 
-# input number
-x = input("Enter a number:")
-y = math.sqrt(float(x))
-print(f"The square root of {x} is {y}")
+# # input number
+# x = input("Enter a number:")
+# y = math.sqrt(float(x))
+# print(f"The square root of {x} is {y}")
+
+name = "Sushmita"
+age = 25
+height = 5.4
+is_student = True
+
+print(name, age, height, is_student)
 
