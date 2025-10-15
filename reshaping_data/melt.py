d37122d7aa0228f2de48bb5ca3464098ae8e186c
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-10-15 21:33:27 +0545

    used pivort  which conver fro long back to wide

--- reshaping_data/melt.py
+++ reshaping_data/melt.py
@@ -13,3 +13,7 @@ melted = pd.melt(df, id_vars=['Name'],
                  var_name='Subject', 
                  value_name='Score')
 print("After Melt (Long Format):\n", melted)
+
+# Pivot: convert from long back to wide
+pivoted = melted.pivot(index='Name', columns='Subject', values='Score')
+print("\nAfter Pivot (Back to Wide Format):\n", pivoted)
