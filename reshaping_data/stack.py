b7edd6314efae1cd86939e595f71fb96a215400e
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-10-15 21:36:33 +0545

    used stack  moves columns to row level

---
+++ reshaping_data/stack.py
@@ -0,0 +1,12 @@
+import pandas as pd
+# Create a small example DataFrame
+df2 = pd.DataFrame({
+    'A': [1, 2],
+    'B': [3, 4]
+}, index=['x', 'y'])
+
+print("\nOriginal DataFrame:\n", df2)
+
+# Stack: moves columns to row level
+stacked = df2.stack()
+print("\nAfter Stack:\n", stacked)
