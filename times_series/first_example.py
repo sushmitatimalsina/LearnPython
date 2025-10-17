6686adf91efef6862120432f1f71cf3ba07d32d2
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-10-17 22:19:29 +0545

    example for times series

---
+++ times_series/first_example.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = {
+    'date': ['2025-10-01', '2025-10-02', '2025-10-03', '2025-10-04', '2025-10-05'],
+    'sales': [200, 220, 250, 230, 210]
+}
+
+df = pd.DataFrame(data)
+
+df['date'] = pd.to_datetime(df['date'])
+
+df.set_index('date', inplace=True)
+weekly_sales = df.resample('W').sum()
+print("Weekly Sales:\n", weekly_sales)
