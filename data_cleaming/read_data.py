ee52efdee0c2fa5fc7f23f6d7ae2823baec17e72
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-10-29 16:07:18 +0545

    print first few rows of the dataframe

--- data_cleaming/read_data.py
+++ data_cleaming/read_data.py
@@ -2,4 +2,10 @@ import pandas as pd
 
 df = pd.read_csv('output_dict.csv')
 print("Original DataFrame:\n", df)
+print("======")
+# Display the first few rows of the DataFrame
 
+print(df.head())
+print("======")
+# Display summary statistics of the DataFrame
+print(df.describe())
