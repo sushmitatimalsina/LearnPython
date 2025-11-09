ecf4da6b387fe3bfb8cf5acbd30a4511288f47a9
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-11-09 21:33:42 +0545

    feat: practices te if-elif-else statement

--- condition.py
+++ condition.py
@@ -67,4 +67,13 @@ if age >= 18:
 else:
        print("You are not eligible to vote.")    
 
+# if-elif-else statement     
+score = 85
+if score >= 90:
+      print("Grade: A")
+elif score >=80:
+      print("Grade: B") 
+else:
+      print("Grade: C")           
+
 
