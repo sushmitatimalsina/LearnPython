b828d86afbfeca197d45a10fd957ed59b52d7fa3
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-11-09 21:29:16 +0545

    feat: practices the if statement

--- condition.py
+++ condition.py
@@ -1,62 +1,70 @@
-# a = 33
-# b = 33
-# if b > a:
-#     print("b is grater than a")
+# # a = 33
+# # b = 33
+# # if b > a:
+# #     print("b is grater than a")
 
-#     # elif- if previous condition werenot true then try this conditions
-# elif a == b:
-#     print("a and b are equal")
+# #     # elif- if previous condition werenot true then try this conditions
+# # elif a == b:
+# #     print("a and b are equal")
 
-# fruits = ["apple", "banana", "cherry"]
-# for x in fruits:
-#   if x == "banana":
-#     continue
-#   print(x)
+# # fruits = ["apple", "banana", "cherry"]
+# # for x in fruits:
+# #   if x == "banana":
+# #     continue
+# #   print(x)
 
-# for m in range(10):
-#    print(m)
-# else:
-#    print("finally finish")
+# # for m in range(10):
+# #    print(m)
+# # else:
+# #    print("finally finish")
 
-# for n in range(2, 30, 3):
-#    if n == 8: continue
-#    print(n)
-# else:
-#    print("finally finished")
+# # for n in range(2, 30, 3):
+# #    if n == 8: continue
+# #    print(n)
+# # else:
+# #    print("finally finished")
 
-#    adj = ["red", "big", "tasty"]
-#    for j in adj:
-#       for k in fruits:
-#          print(j, k)
-numbers = [12, 45, 7, 23, 89, 5, 33]
+# #    adj = ["red", "big", "tasty"]
+# #    for j in adj:
+# #       for k in fruits:
+# #          print(j, k)
+# numbers = [12, 45, 7, 23, 89, 5, 33]
 
-max_num = numbers[0]
-for num in numbers:
-    if num > max_num:
-        max_num = num 
-print("Maximum number:", max_num)        
+# max_num = numbers[0]
+# for num in numbers:
+#     if num > max_num:
+#         max_num = num 
+# print("Maximum number:", max_num)        
 
-min_num = numbers[0]
-for num1 in numbers:
-    if num1 < min_num:
-        min_num = num1
-print("Minimum number:", min_num)        
+# min_num = numbers[0]
+# for num1 in numbers:
+#     if num1 < min_num:
+#         min_num = num1
+# print("Minimum number:", min_num)        
 
-total = 0
-for num2 in numbers:
-      total += num2
-print("Sum of all numbers:", total)    
+# total = 0
+# for num2 in numbers:
+#       total += num2
+# print("Sum of all numbers:", total)    
 
-sorted_number = numbers.copy()
-for i in range(len(sorted_number)):
-    for j in range(i+1, len(sorted_number)):
-        if sorted_number[i] > sorted_number[j]:
-            sorted_number[i], sorted_number[j] = sorted_number[j], sorted_number[i]
-print("Sorted numbers:", sorted_number)           
+# sorted_number = numbers.copy()
+# for i in range(len(sorted_number)):
+#     for j in range(i+1, len(sorted_number)):
+#         if sorted_number[i] > sorted_number[j]:
+#             sorted_number[i], sorted_number[j] = sorted_number[j], sorted_number[i]
+# print("Sorted numbers:", sorted_number)           
 
-reversed_numbers = []
-for m in range(len(numbers)-1, -1, -1):
-      reversed_numbers.append(numbers[m])
-print("Reversed numbers:", reversed_numbers)   
+# reversed_numbers = []
+# for m in range(len(numbers)-1, -1, -1):
+#       reversed_numbers.append(numbers[m])
+# print("Reversed numbers:", reversed_numbers)  
+# 
+# if statement
+# 
+age = 18
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+       print("You are not eligible to vote.")    
 
 
