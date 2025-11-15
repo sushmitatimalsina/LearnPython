fbcdd587851fb167f6be3573b1d0de6fdb0a72b3
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-11-15 22:03:54 +0545

    refactor: uncomment and reorganize array example code

--- array_1.py
+++ array_1.py
@@ -1,10 +1,12 @@
+# numbers = [10, 20, 30, 40, 50]
+# print(numbers[0])
+
+# numbers[2] = 35
+# print(numbers)
+
+# for num in numbers:
+#     print(num)
+
 numbers = [10, 20, 30, 40, 50]
-print(numbers[0])
-
-numbers[2] = 35
 print(numbers)
-
-for num in numbers:
-    print(num)
-
     
