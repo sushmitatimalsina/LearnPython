b96858a9821ebbc6943440b02ec7abea226d4abb
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-11-17 20:48:01 +0545

    feat: comment out initial array and print statements for clarity

--- NumPy_practice/array.py
+++ NumPy_practice/array.py
@@ -1,14 +1,17 @@
 import numpy as np
 
-arr = np.array([1,2,3,4,5])
-print(arr)
+# arr = np.array([1,2,3,4,5])
+# print(arr)
 
-arr2D = np.array([[1,2],[4,6]])
-print(arr2D)
-print(arr2D[0,1])
-print(arr2D[:, 1] )
-print(arr2D[1, :]  )
+# arr2D = np.array([[1,2],[4,6]])
+# print(arr2D)
+# print(arr2D[0,1])
+# print(arr2D[:, 1] )
+# print(arr2D[1, :]  )
 
-print(np.zeros((3, 3)))
+# print(np.zeros((3, 3)))
 
-print(np.arange(1, 11))
+# print(np.arange(1, 11))
+
+arr = np.array([1, 2, 3])
+print(arr + 5)
