288814a6b8aaf232a8995b4e30dc78d1ef237801
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-11-20 19:55:15 +0545

    feat: practices the function

--- function.py
+++ function.py
@@ -2,38 +2,42 @@
 
 #  print("hello ")
 
-# my_function()
+# # my_function()
 
-def details(fname):
-    print(fname + " Timalsina")   
+# def details(fname):
+#     print(fname + " Timalsina")   
 
-details("Sushmita")
-details("Sushil")
-details("Saraswoti")
-details("Govinda")
+# details("Sushmita")
+# details("Sushil")
+# details("Saraswoti")
+# details("Govinda")
 
-def student(name,sirname):
-    print(name + " " + sirname)
+# def student(name,sirname):
+#     print(name + " " + sirname)
 
-student("Asmita", "Timalsina")    
+# student("Asmita", "Timalsina")    
 
-def fruits(*name):
-    print("My fav fruits is " + name[2])
-fruits("apple", "banana", "Mango")
+# def fruits(*name):
+#     print("My fav fruits is " + name[2])
+# fruits("apple", "banana", "Mango")
 
-def child(child3, child2, child1):
-    print("The younger child is " + child3)
+# def child(child3, child2, child1):
+#     print("The younger child is " + child3)
 
-child(child1 = "Emil", child2 = "Tobias", child3 = "Linus")    
+# child(child1 = "Emil", child2 = "Tobias", child3 = "Linus")    
 
-def hi(food):
-    for x in food:
-        print(x)
-game = ("a", "b", "c")
-hi(game)
+# def hi(food):
+#     for x in food:
+#         print(x)
+# game = ("a", "b", "c")
+# hi(game)
 
-def add(x):
-    return 5 + x
-# print(add(5))
-print("Sum is " + str(add(10)))
+# def add(x):
+#     return 5 + x
+# # print(add(5))
+# print("Sum is " + str(add(10)))
+
+def greet(name="User"):
+    return f"Hello, {name}!"
+print(greet()) 
  
