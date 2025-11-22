71b8924f04ddc4096fe1f5a9f6691b9421faa0be
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-11-22 20:09:58 +0545

    feat: practices the read the excel file

--- file_handling/read_file.py
+++ file_handling/read_file.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
-with open("notes.txt", "r") as file:
-    content = file.read()
-print(content)    
+# with open("notes.txt", "r") as file:
+#     content = file.read()
+# print(content)    
+
+df = pd.read_excel(r"D:\python\python\file_handling\output.xlsx")
+print(df)
