bf7c89a2fddb2b9d7a105b8d999c5bd2070ebb29
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-11-22 19:47:53 +0545

    feat: practices the append in the same file

--- file_handling/write.py
+++ file_handling/write.py
@@ -4,4 +4,6 @@ with open("notes.txt", "w") as file:
     file.write("Hello, this is my first data engineering file! \n")
     file.write("I am someone who likes learning new technologies and improving my skills every day. " \
     "I enjoy building apps, solving problems, and working on practical projects. " \
-    "I am also trying to become better in data engineering and development by creating real-life solutions.")
+    "I am also trying to become better in data engineering and development by creating real-life solutions.\n")
+with open("notes.txt", "a") as file:
+    file.write("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii !")   
