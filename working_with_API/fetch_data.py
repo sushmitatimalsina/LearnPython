c757092f96f6a62c78d127e68b6d0c450cac4928
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-11-23 20:12:02 +0545

    feat: Add API fetch

---
+++ working_with_API/fetch_data.py
@@ -0,0 +1,21 @@
+import requests
+import pandas as pd
+
+url = "https://jsonplaceholder.typicode.com/users"
+response = requests.get(url)
+data = response.json()
+print(data[0])
+
+users = []
+
+for item in data:
+    users.append({
+        "ID": item["id"],
+        "Name": item["name"],
+        "Username": item["username"],
+        "Email": item["email"],
+        "City": item["address"]["city"]
+    })
+
+df = pd.DataFrame(users)
+print(df)
