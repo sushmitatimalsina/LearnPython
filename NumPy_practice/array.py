970129a3067e0dd2d552053899546a7454ae8db4
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-11-26 20:42:23 +0545

    faet: practices the array

--- NumPy_practice/array.py
+++ NumPy_practice/array.py
@@ -1,43 +1,54 @@
 import numpy as np
 
-# arr = np.array([1,2,3,4,5])
-# print(arr)
+# # arr = np.array([1,2,3,4,5])
+# # print(arr)
 
-# arr2D = np.array([[1,2],[4,6]])
-# print(arr2D)
-# print(arr2D[0,1])
-# print(arr2D[:, 1] )
-# print(arr2D[1, :]  )
+# # arr2D = np.array([[1,2],[4,6]])
+# # print(arr2D)
+# # print(arr2D[0,1])
+# # print(arr2D[:, 1] )
+# # print(arr2D[1, :]  )
 
-# print(np.zeros((3, 3)))
-# print(np.ones((2, 4)))
+# # print(np.zeros((3, 3)))
+# # print(np.ones((2, 4)))
 
-# print(np.arange(1, 11))
+# # print(np.arange(1, 11))
 
-# arr = np.array([1, 2, 3])
-# print(arr + 5)
+# # arr = np.array([1, 2, 3])
+# # print(arr + 5)
 
-# print(arr * 2)
-# print(arr / 2)
+# # print(arr * 2)
+# # print(arr / 2)
 
-# a = np.array([1, 2, 3])
-# b = np.array([4, 5, 6])
-# print(a + b)  
-# print(a * b) 
+# # a = np.array([1, 2, 3])
+# # b = np.array([4, 5, 6])
+# # print(a + b)  
+# # print(a * b) 
 
-# arr = np.arange(1, 10)
-# arr.reshape(3, 3)
-# print(arr)
+# # arr = np.arange(1, 10)
+# # arr.reshape(3, 3)
+# # print(arr)
 
-# print(np.linspace(0, 1, 5))
+# # print(np.linspace(0, 1, 5))
 
-# arr = np.array([[1, 2, 3], [4, 5, 6]])
-# # arr.shape
-# print(arr.shape)
-# print(arr.size)
+# # arr = np.array([[1, 2, 3], [4, 5, 6]])
+# # # arr.shape
+# # print(arr.shape)
+# # print(arr.size)
 
 
-# print(np.random.rand(3))
-print(np.random.randint(1, 100, 5))
+# # print(np.random.rand(3))
+# print(np.random.randint(1, 100, 5))
 
-print(np.random.rand(3, 3))
+# print(np.random.rand(3, 3))
+
+arr = np.array([1, 2, 3, 4, 5])
+
+# Arithmetic operations
+print("Add 5:", arr + 5)
+print("Multiply by 2:", arr * 2)
+
+# Sum, Mean, Max
+print("Sum:", arr.sum())
+print("Mean:", arr.mean())
+print("Max:", arr.max())
