7e7a507f8708e93bb797b96b0e3cd5ac24d5e2d4
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-11-30 16:48:26 +0545

    refactor: update fetch_data.py to process comments and remove post handling

--- working_with_API/fetch_data.py
+++ working_with_API/fetch_data.py
@@ -23,22 +23,51 @@ import pandas as pd
 # df.to_excel("api_users.xlsx", index=False)
 # print("Data saved to 'api_users.xlsx'")
 
-url = "https://jsonplaceholder.typicode.com/posts"
-response  = requests.get(url)
+# url = "https://jsonplaceholder.typicode.com/posts"
+# response  = requests.get(url)
+# data = response.json()
+# print(data[0])
+
+# posts = []
+# for item in data:
+#     posts.append({
+#         "UserID": item["userId"],
+#         "PostID": item["id"],
+#         "Title": item["title"],
+#         "Body": item["body"]
+#     })
+
+# df = pd.DataFrame(posts)
+# print(df)
+# df.to_csv("api_posts.csv", index=False)
+# print("Data saved to 'api_posts.csv'")  
+
+url = "https://jsonplaceholder.typicode.com/comments"
+response = requests.get(url)
 data = response.json()
 print(data[0])
 
-posts = []
+comments = []
 for item in data:
-    posts.append({
-        "UserID": item["userId"],
-        "PostID": item["id"],
-        "Title": item["title"],
+    comments.append({
+        "PostID": item["postId"],
+        "CommentID": item["id"],
+        "Name": item["name"],
+        "Email": item["email"],
         "Body": item["body"]
     })
 
-df = pd.DataFrame(posts)
+df = pd.DataFrame(comments)
 print(df)
-df.to_csv("api_posts.csv", index=False)
-print("Data saved to 'api_posts.csv'")  
+df.to_json("api_comments.json", orient="records", lines=True)
+print("Data saved to 'api_comments.json'")
+
+
+df_html = df.to_html()
+with open("api_comments.html", "w") as file:
+    file.write(df_html)
+print("Data saved to 'api_comments.html'")
+df.to_excel("api_comments.xlsx", index=False)
+print("Data saved to 'api_comments.xlsx'")
+
 
