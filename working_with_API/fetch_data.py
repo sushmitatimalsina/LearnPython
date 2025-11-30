181ec7d69b4b487ab46aedfb7beab3f2d3a23cb0
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-11-30 16:36:29 +0545

    feat: update fetch_data.py to retrieve and save posts data from API

--- working_with_API/fetch_data.py
+++ working_with_API/fetch_data.py
@@ -1,24 +1,44 @@
 import requests
 import pandas as pd
 
-url = "https://jsonplaceholder.typicode.com/users"
-response = requests.get(url)
+# url = "https://jsonplaceholder.typicode.com/users"
+# response = requests.get(url)
+# data = response.json()
+# print(data[0])
+
+# users = []
+
+# for item in data:
+#     users.append({
+#         "ID": item["id"],
+#         "Name": item["name"],
+#         "Username": item["username"],
+#         "Email": item["email"],
+#         "City": item["address"]["city"]
+#     })
+
+# df = pd.DataFrame(users)
+# print(df)
+
+# df.to_excel("api_users.xlsx", index=False)
+# print("Data saved to 'api_users.xlsx'")
+
+url = "https://jsonplaceholder.typicode.com/posts"
+response  = requests.get(url)
 data = response.json()
 print(data[0])
 
-users = []
-
+posts = []
 for item in data:
-    users.append({
-        "ID": item["id"],
-        "Name": item["name"],
-        "Username": item["username"],
-        "Email": item["email"],
-        "City": item["address"]["city"]
+    posts.append({
+        "UserID": item["userId"],
+        "PostID": item["id"],
+        "Title": item["title"],
+        "Body": item["body"]
     })
 
-df = pd.DataFrame(users)
+df = pd.DataFrame(posts)
 print(df)
+df.to_csv("api_posts.csv", index=False)
+print("Data saved to 'api_posts.csv'")  
 
-df.to_excel("api_users.xlsx", index=False)
-print("Data saved to 'api_users.xlsx'")
