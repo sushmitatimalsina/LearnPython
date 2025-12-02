de9a29e606224552f8e51028a5aa27c87a14089e
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-12-02 17:22:32 +0545

    feat: create the dataframe

--- pandas_example/dataFrame/pandas_example.py
+++ pandas_example/dataFrame/pandas_example.py
@@ -1,16 +1,25 @@
 import pandas as pd
 
-data = {
-    "Name":[ "John", "Anna", "Peter", "Linda" ],
-    "Age":[ 28, 24, 35, 32 ],
-    "City":[ "New York", "Paris", "Berlin", "London" ],
-    "salary": [50000, 60000, 55000, 65000]
+# data = {
+#     "Name":[ "John", "Anna", "Peter", "Linda" ],
+#     "Age":[ 28, 24, 35, 32 ],
+#     "City":[ "New York", "Paris", "Berlin", "London" ],
+#     "salary": [50000, 60000, 55000, 65000]
 
+# }
+# df = pd.DataFrame(data)
+# print(df)
+
+# print("===========================")
+# print(df.head(2))  # First 2 rows
+# print("===========================")
+# print(df.tail(2))  # Last 2 rows
+
+data = {
+    "Name": ["Ram", "Sita", "Hari"],
+    "Age": [25, 22, 28],
+    "City": ["Kathmandu", "Pokhara", "Lalitpur"]
 }
+
 df = pd.DataFrame(data)
 print(df)
-
-print("===========================")
-print(df.head(2))  # First 2 rows
-print("===========================")
-print(df.tail(2))  # Last 2 rows
