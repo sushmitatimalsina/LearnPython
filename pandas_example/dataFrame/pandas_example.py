a3815e652276db42b94f06995807ee5d12267acd
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-12-02 19:09:02 +0545

    create a new data frame

--- pandas_example/dataFrame/pandas_example.py
+++ pandas_example/dataFrame/pandas_example.py
@@ -1,28 +1,50 @@
 import pandas as pd
 
-# data = {
-#     "Name":[ "John", "Anna", "Peter", "Linda" ],
-#     "Age":[ 28, 24, 35, 32 ],
-#     "City":[ "New York", "Paris", "Berlin", "London" ],
-#     "salary": [50000, 60000, 55000, 65000]
+# # data = {
+# #     "Name":[ "John", "Anna", "Peter", "Linda" ],
+# #     "Age":[ 28, 24, 35, 32 ],
+# #     "City":[ "New York", "Paris", "Berlin", "London" ],
+# #     "salary": [50000, 60000, 55000, 65000]
 
+# # }
+# # df = pd.DataFrame(data)
+# # print(df)
+
+# # print("===========================")
+# # print(df.head(2))  # First 2 rows
+# # print("===========================")
+# # print(df.tail(2))  # Last 2 rows
+
+# data = {
+#     "Name": ["Ram", "Sita", "Hari"],
+#     "Age": [25, 22, 28],
+#     "City": ["Kathmandu", "Pokhara", "Lalitpur"]
 # }
+
 # df = pd.DataFrame(data)
 # print(df)
 
-# print("===========================")
-# print(df.head(2))  # First 2 rows
-# print("===========================")
-# print(df.tail(2))  # Last 2 rows
+# df = pd.read_csv("new_students.csv")
+# print(df.head())
+
+
 
 data = {
-    "Name": ["Ram", "Sita", "Hari"],
-    "Age": [25, 22, 28],
-    "City": ["Kathmandu", "Pokhara", "Lalitpur"]
+    "user_id": [1, 2, 3, 4, 5],
+    "name": ["Sushmita", "Alex", "Priya", "John", "Mina"],
+    "email": [
+        "sushmita@example.com",
+        "alex@example.com",
+        "priya@example.com",
+        "john@example.com",
+        "mina@example.com"
+    ],
+    "age": [23, 30, 27, None, 25],
+    "city": ["Kathmandu", "Pokhara", "Butwal", "Kathmandu", None]
 }
 
 df = pd.DataFrame(data)
+
 print(df)
 
-df = pd.read_csv("new_students.csv")
-print(df.head())
+
