eeb6729bf3f1991c10bbc37dbc2d19db060e689a
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-12-03 17:11:39 +0545

    feat: saved the data in the csv file

--- dataset/data.py
+++ dataset/data.py
@@ -1,3 +1,6 @@
+import csv
+import json
+
 users = [
     {"id": 1, "name": "John", "email": "john@example.com"},
     {"id": 2, "name": "Alice", "email": "alice@example.com"}
@@ -5,3 +8,16 @@ users = [
 
 for user in users:
     print(f"{user['id']} - {user['name']} - {user['email']}")
+
+with open("users.csv", "w", newline="") as file:
+    writer = csv.DictWriter(file, fieldnames=["id", "name", "email"])
+    writer.writeheader()
+    writer.writerows(users)
+
+with open("users.csv") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        print(row)
+
+with open("users.json", "w") as file:
+    json.dump(users, file, indent=4)
