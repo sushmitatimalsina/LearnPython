fa277bc5890671f273df561a1cfe06a71979fefb
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-12-03 17:04:42 +0545

    feat: format the documents

--- array_1.py
+++ array_1.py
@@ -1,18 +1,18 @@
-# numbers = [10, 20, 30, 40, 50]
-# print(numbers[0])
+# # numbers = [10, 20, 30, 40, 50]
+# # print(numbers[0])
 
-# numbers[2] = 35
+# # numbers[2] = 35
+# # print(numbers)
+
+# # for num in numbers:
+# #     print(num)
+
+# numbers = [10, 20, 30, 40, 50]
 # print(numbers)
 
-# for num in numbers:
-#     print(num)
-
-numbers = [10, 20, 30, 40, 50]
-print(numbers)
-
-print(numbers[0])
-print(numbers[3])  
-numbers.append(60)
-print(numbers)
-numbers.remove(20)
-print(numbers)
+# print(numbers[0])
+# print(numbers[3])  
+# numbers.append(60)
+# print(numbers)
+# numbers.remove(20)
+# print(numbers)
