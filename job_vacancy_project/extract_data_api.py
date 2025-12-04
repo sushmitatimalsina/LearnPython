256c9dadb5164a13c53e3e91d1143d51e5f3ed2b
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-12-04 22:21:51 +0545

    feat: tried to added the data into the mysql

--- job_vacancy_project/extract_data_api.py
+++ job_vacancy_project/extract_data_api.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+import mysql.connector
 
 url = "https://jsonplaceholder.typicode.com/posts"
 response = requests.get(url)
@@ -20,3 +21,22 @@ df["description_length"] = df["job_description"].apply(lambda x: len(x))
 df_clean = df.drop_duplicates()
 df_clean = df_clean[df_clean["description_length"]>20]
 print(df_clean.info())
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="job_data"
+)
+
+cursor = conn.cursor()
+for _, row in df_clean.iterrows():
+    cursor.execute("""
+        INSERT INTO jobs (job_id, company_id, job_title, job_description, description_length)
+        VALUES (%s, %s, %s, %s, %s)
+    """, tuple(row))
+
+conn.commit()
+conn.close()
+
+print("\nData loaded into MySQL successfully!")
