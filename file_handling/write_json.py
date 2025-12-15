c19354696ee252d6a0dc10092fa3558460a6f714
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-12-15 22:54:42 +0545

    feat: added the file for the json file

--- file_handling/write_json.py
+++ file_handling/write_json.py
@@ -1,18 +1,20 @@
 import pandas as pd
 import json
 
-employee = {
-    "name": "Ram",
-    "role": "Data Analyst",
-    "country": "Nepal"
-}
+# employee = {
+#     "name": "Ram",
+#     "role": "Data Analyst",
+#     "country": "Nepal"
+# }
 
-with open("employee.json", "w") as file:
-    json.dump(employee, file, indent=4)
+# with open("employee.json", "w") as file:
+#     json.dump(employee, file, indent=4)
 
 countries = {
     "countries": ["Nepal", "India", "USA", "UK"]
 }
 
 with open("countries.json", "w") as file:
-    json.dump(countries, file, indent=4)    
+    json.dump(countries, file, indent=4)  
+
+     
