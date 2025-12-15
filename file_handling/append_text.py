f579c4b9aa833495780c8435628093da34299115
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-12-15 22:17:20 +0545

    feat: implemented the append oeration

---
+++ file_handling/append_text.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+with open("data.txt", "a") as file:
+    file.write("\nAppending new line 1.")
+    file.write("\nAppending new line 2.")
+    file.write("\nAppending new line 3.")
