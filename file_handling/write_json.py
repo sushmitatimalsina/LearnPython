3d325e174b11b14142a17b6ee96aaf93e87dc4ba
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-12-15 22:45:24 +0545

    feat: Generate countries dataset in JSON format

--- file_handling/write_json.py
+++ file_handling/write_json.py
@@ -9,3 +9,10 @@ employee = {
 
 with open("employee.json", "w") as file:
     json.dump(employee, file, indent=4)
+
+countries = {
+    "countries": ["Nepal", "India", "USA", "UK"]
+}
+
+with open("countries.json", "w") as file:
+    json.dump(countries, file, indent=4)    
