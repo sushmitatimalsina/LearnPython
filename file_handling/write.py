abaa33d4cdd0582d3c5801398ad0021b20bed874
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-12-15 22:54:13 +0545

    feat: added the file for the countries

--- file_handling/write.py
+++ file_handling/write.py
@@ -1,33 +1,36 @@
-# import pandas as pd
-
-# with open("notes.txt", "w") as file:
-#     file.write("Hello, this is my first data engineering file! \n")
-#     file.write("I am someone who likes learning new technologies and improving my skills every day. " \
-#     "I enjoy building apps, solving problems, and working on practical projects. " \
-#     "I am also trying to become better in data engineering and development by creating real-life solutions.\n")
-# with open("notes.txt", "a") as file:
-#     file.write("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii !")   
-
+import pandas as pd
 import csv
-import json
+
+# # with open("notes.txt", "w") as file:
+# #     file.write("Hello, this is my first data engineering file! \n")
+# #     file.write("I am someone who likes learning new technologies and improving my skills every day. " \
+# #     "I enjoy building apps, solving problems, and working on practical projects. " \
+# #     "I am also trying to become better in data engineering and development by creating real-life solutions.\n")
+# # with open("notes.txt", "a") as file:
+# #     file.write("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii !")   
+
+# import csv
+# import json
 
 
-# countries = {
-#     "countries": ["Nepal", "India", "USA", "UK"]
-# }
+# # countries = {
+# #     "countries": ["Nepal", "India", "USA", "UK"]
+# # }
 
-# Read text file
-with open("countries.txt", "r") as file:
-    countries = [line.strip() for line in file]
+# # Read text file
+# # with open("countries.txt", "r") as file:
+# #     countries = [line.strip() for line in file]
 
-# Write CSV
-with open("countries.csv", "w", newline="") as file:
-    writer = csv.writer(file)
-    writer.writerow(["country"])
-    for c in countries:
-        writer.writerow([c])
+# # Write CSV
+# with open("countries.csv", "w", newline="") as file:
+#     writer = csv.writer(file)
+#     writer.writerow(["country"])
+#     for c in countries:
+#         writer.writerow([c])
 
-# Write JSON
-with open("countries.json", "w") as file:
-    json.dump({"countries": countries}, file, indent=4)
+# # Write JSON
+# with open("countries.json", "w") as file:
+#     json.dump({"countries": countries}, file, indent=4)
+
+
 
