db5e3a1dcdcaacb639b8be6ffa754f64e8c02a5a
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-12-15 22:55:31 +0545

    feat: added the file for the converting the json file to text

--- file_handling/read_csv.py
+++ file_handling/read_csv.py
@@ -1,7 +1,19 @@
 import pandas as pd
 import csv
+import json
 
-with open("output.csv", "r") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        print(row["order_id"], row["amount"], row["country"])
+# with open("output.csv", "r") as file:
+#     reader = csv.DictReader(file)
+#     for row in reader:
+#         print(row["order_id"], row["amount"], row["country"])
+
+
+with open("countries.json", "r") as file:
+    data = json.load(file)
+
+countries = data["countries"]
+with open("countries_converted.csv", "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["country"])  # Header
+    for c in countries:
+        writer.writerow([c])    
