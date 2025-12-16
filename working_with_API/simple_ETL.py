84cc2f4ee6d76b2ef8485dcb5980f42150204313
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-12-16 21:28:28 +0545

    feat: used the simple ETL using API

---
+++ working_with_API/simple_ETL.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import requests
+
+response = requests.get("https://jsonplaceholder.typicode.com/posts")
+data = response.json()
+
+df = pd.DataFrame(data)
+df = df[["userId", "title"]]
+
+df.to_csv("posts.csv", index=False)
+print("ETL from API completed")
