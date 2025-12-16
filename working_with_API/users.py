cc6a705b27b213d96d962191cb32302209446cc3
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-12-16 21:47:33 +0545

    feat: transform the data in clean and modify it

--- working_with_API/users.py
+++ working_with_API/users.py
@@ -5,12 +5,19 @@ url = "https://jsonplaceholder.typicode.com/users"
 response = requests.get(url)
 data = response.json()
 
-print("Fetched Data:")
-table = pd.DataFrame([{
-    "UserID": u["id"],
-    "Name": u["name"].strip().title(),
-    "Username": u["username"].strip(),
-    "Email": u["email"].lower().strip(),
-    "City": u["address"]["city"].strip().title()
-} for u in data])
-print(table)
+# print("Fetched Data:")
+# table = pd.DataFrame([{
+#     "UserID": u["id"],
+#     "Name": u["name"].strip().title(),
+#     "Username": u["username"].strip(),
+#     "Email": u["email"].lower().strip(),
+#     "City": u["address"]["city"].strip().title()
+# } for u in data])
+# print(table)
+
+df = pd.DataFrame(data)
+df = df[["id", "name", "email", "phone"]]
+df = df.rename(columns={"name": "user_name"})
+df = df.drop_duplicates()
+
+
