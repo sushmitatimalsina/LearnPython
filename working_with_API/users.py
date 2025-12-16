4452fa3b6ff1cec50096795d4005b4915ca7bf5e
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-12-16 21:43:45 +0545

    feat: extract the data from the users api

---
+++ working_with_API/users.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import requests
+
+url = "https://jsonplaceholder.typicode.com/users"
+response = requests.get(url)
+data = response.json()
+
+print("Fetched Data:")
+table = pd.DataFrame([{
+    "UserID": u["id"],
+    "Name": u["name"].strip().title(),
+    "Username": u["username"].strip(),
+    "Email": u["email"].lower().strip(),
+    "City": u["address"]["city"].strip().title()
+} for u in data])
+print(table)
