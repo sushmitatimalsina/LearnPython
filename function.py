7c1481dc9f41bfc48a8f93e59f0321565d0def6b
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-12-17 12:44:33 +0545

    feat: converted  to celsius with the help of function

--- function.py
+++ function.py
@@ -297,3 +297,12 @@ def my_function():
     print("Hello from a function")
 
 my_function()
+
+def fahrenheit_to_celsius(f):
+    return(f-32)*5/9
+
+print(fahrenheit_to_celsius(100))
+print(fahrenheit_to_celsius(50))
+
+
+   
