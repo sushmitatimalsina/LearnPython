7c113a5f654ddf488c7227c125a6928850f390cb
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-12-18 22:08:38 +0545

    feat: created the example for the simple list of lists

--- pandas_example/dataFrame/list_of_list.py
+++ pandas_example/dataFrame/list_of_list.py
@@ -1,18 +1,29 @@
 import pandas as pd
 
-# Create data as a list of lists
+# # Create data as a list of lists
+# data = [
+#     [101, 'Aarav', 'Engineering', 60000],
+#     [102, 'Bina', 'HR', 52000],
+#     [103, 'Chirag', 'Finance', 58000],
+#     [104, 'Diya', 'Marketing', 55000]
+# ]
+
+# # Define column names
+# columns = ['Employee_ID', 'Name', 'Department', 'Salary']
+
+# # Create the DataFrame
+# df = pd.DataFrame(data, columns=columns)
+
+# # Display the DataFrame
+# print(df)
+
 data = [
-    [101, 'Aarav', 'Engineering', 60000],
-    [102, 'Bina', 'HR', 52000],
-    [103, 'Chirag', 'Finance', 58000],
-    [104, 'Diya', 'Marketing', 55000]
+    [1, "Alice", "alice@example.com"],
+    [2, "Bob", "bob@example.com"],
+    [3, "Charlie", "charlie@example.com"]
 ]
 
-# Define column names
-columns = ['Employee_ID', 'Name', 'Department', 'Salary']
+print(data[0])
 
-# Create the DataFrame
-df = pd.DataFrame(data, columns=columns)
+print(data[1][1])
 
-# Display the DataFrame
-print(df)
