eb0f555167f4cee53d141ed107f01b1e550257ab
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-12-29 21:49:10 +0545

    feat: created the class with usin the ETL pipline

--- OOP/simple_class.py
+++ OOP/simple_class.py
@@ -43,3 +43,30 @@ users = [user1, user2, user3]
 
 for u in users:
     u.greet()
+
+class ETL:
+    def __init__(self, input_file, output_file):
+        self.input_file = input_file
+        self.output_file = output_file
+        self.data = None
+
+    def extract(self):
+        self.data = pd.read_csv(self.input_file)
+        print("Data extracted")
+
+    def transform(self):
+        # Remove duplicates and fill nulls
+        self.data = self.data.drop_duplicates()
+        self.data["name"] = self.data["name"].fillna("Unknown")
+        self.data["city"] = self.data["city"].fillna("Unknown")
+        print("Data transformed")
+
+    def load(self):
+        self.data.to_csv(self.output_file, index=False)
+        print("Data loaded")
+
+# Using the ETL pipeline
+etl = ETL("users_clean.csv", "users_final.csv")
+etl.extract()
+etl.transform()
+etl.load()    
