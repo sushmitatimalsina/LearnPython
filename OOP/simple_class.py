4a724dd96370e059554161e8dbf071d8de5fedcc
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-12-29 21:46:23 +0545

    feat: created the users as the class

--- OOP/simple_class.py
+++ OOP/simple_class.py
@@ -1,24 +1,37 @@
 import pandas as pd
 
-class DataProcessor:
-    def __init__(self, file_path):
-        self.file_path = file_path
-        self.df = None
+# class DataProcessor:
+#     def __init__(self, file_path):
+#         self.file_path = file_path
+#         self.df = None
 
-    def load_data(self):
-        self.df = pd.read_csv(self.file_path)
-        print("data loaded successfully")
-        return self.df 
+#     def load_data(self):
+#         self.df = pd.read_csv(self.file_path)
+#         print("data loaded successfully")
+#         return self.df 
 
-    def clean_data(self):
-        # Example: Drop missing values
-        self.df = self.df.dropna()
-        print("Missing values dropped")
-        return self.df  
+#     def clean_data(self):
+#         # Example: Drop missing values
+#         self.df = self.df.dropna()
+#         print("Missing values dropped")
+#         return self.df  
         
-    def show_head(self, n=5):
-        print(self.df.head(n))   
-processor = DataProcessor("users.csv")
-processor.load_data()
-processor.clean_data()
-processor.show_head()        
+#     def show_head(self, n=5):
+#         print(self.df.head(n))   
+# processor = DataProcessor("users.csv")
+# processor.load_data()
+# processor.clean_data()
+# processor.show_head()   
+# 
+# 
+class User:
+    def __init__(self, name, email):
+        self.name = name       # attribute
+        self.email = email
+
+    def greet(self):           # method
+        print(f"Hello, my name is {self.name}")
+
+# Create object
+user1 = User("Ram", "ram@gmail.com")
+user1.greet()     
