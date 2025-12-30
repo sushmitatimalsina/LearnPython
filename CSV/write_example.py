764984deed221abf2dea8876fc69c4a04880e600
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-12-30 21:34:12 +0545

    feat: update write_example.py to include DataFrame creation and remove unused code

--- CSV/write_example.py
+++ CSV/write_example.py
@@ -1,21 +1,32 @@
 import csv
+import pandas as pd
 
-data = [
-    {"Name": "Alice", "Age": 25, "City": "New York"},
-    {"Name": "Bob", "Age": 30, "City": "Los Angeles"},
-    {"Name": "Charlie", "Age": 22, "City": "Chicago"},
-    {"Name": "Alice", "Age": 25, "City": "New York"},
-    {"Name": "Bob", "Age": 30, "City": "Los Angeles"},
-    {"Name": "Charlie", "Age": 22, "City": "Chicago"}
+# data = [
+#     {"Name": "Alice", "Age": 25, "City": "New York"},
+#     {"Name": "Bob", "Age": 30, "City": "Los Angeles"},
+#     {"Name": "Charlie", "Age": 22, "City": "Chicago"},
+#     {"Name": "Alice", "Age": 25, "City": "New York"},
+#     {"Name": "Bob", "Age": 30, "City": "Los Angeles"},
+#     {"Name": "Charlie", "Age": 22, "City": "Chicago"}
 
-]
+# ]
 
-fieldnames = ["Name", "Age", "City"]
+# fieldnames = ["Name", "Age", "City"]
 
-with open ("output_dict.csv", mode="w", newline="") as file:
-    writer = csv.DictWriter(file, fieldnames=fieldnames)
-    writer.writeheader()
-    writer.writerows(data)
+# with open ("output_dict.csv", mode="w", newline="") as file:
+#     writer = csv.DictWriter(file, fieldnames=fieldnames)
+#     writer.writeheader()
+#     writer.writerows(data)
 
 
-print("CSV file (dictionary style) has been written successfully!")    
+# print("CSV file (dictionary style) has been written successfully!")   
+# 
+data = {
+    "user_id": [1, 2, 2, 3, 4],
+    "name": ["Ram", "Sita", "Sita", None, "Hari"],
+    "city": ["Kathmandu", "Banepa", "Banepa", "Pokhara", None]
+}
+
+df = pd.DataFrame(data)
+print("Original data:")
+print(df) 
