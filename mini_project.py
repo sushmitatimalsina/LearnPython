e5bf292946527f13d76e5ad324a5644ab0e84489
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-12-30 21:50:42 +0545

    feat: Keep only useful columns in the dataframe

--- mini_project.py
+++ mini_project.py
@@ -5,3 +5,10 @@ url = "https://jsonplaceholder.typicode.com/users"
 response = requests.get(url)
 data = response.json()
 print(f"Fetched {len(data)} users from API")
+df = pd.DataFrame(data)
+df = df[["id", "name", "email", "address"]]
+# df["city"] = df["address"].apply(lambda x: x["city"])
+# df = df.drop(columns=["address"])
+
+# print("\nRaw data:")
+# print(df.head())
