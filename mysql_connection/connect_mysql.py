f31d6325d2ab5c9566b8930ea3baf63294e1c7d9
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-12-31 11:11:22 +0545

    feat: Update MySQL connection to use pymysql and improve error handling

--- mysql_connection/connect_mysql.py
+++ mysql_connection/connect_mysql.py
@@ -1,19 +1,23 @@
-import mysql.connector
+import pymysql
 
 def get_mysql_connection():
     try:
-        conn = mysql.connector.connect(
+        conn = pymysql.connect(
             host="localhost",
             user="root",
             password="root123",
             database="testdb"
         )
 
-        if conn.is_connected():
-            print("Connected to MySQL successfully")
-            return conn
+        print("Connected to MySQL successfully")
+        return conn
 
-    except mysql.connector.Error as e:
+    except pymysql.MySQLError as e:
         print("Error while connecting to MySQL:", e)
         return None
-    
+
+conn = get_mysql_connection()
+
+if conn:
+    conn.close()
+    print("MySQL connection closed")
