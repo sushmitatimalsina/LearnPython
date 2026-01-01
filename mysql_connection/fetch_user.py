908354417f72daa2a37f38e08fb43c5d56119a6b
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2026-01-01 17:23:11 +0545

    feat: Remove success message from fetch_api_data and ensure cursor closure in create_table

--- mysql_connection/fetch_user.py
+++ mysql_connection/fetch_user.py
@@ -22,7 +22,6 @@ def fetch_api_data():
         response.raise_for_status()
         return response.json()
     
-        print("API data fetched successfully")
     except requests.RequestException as e:
         print("Error fetching API data:", e)
         return []
@@ -47,12 +46,38 @@ def create_table(conn):
         print("create table error:", e)
 
     finally:
+        cursor.close()  
+
+def insert_api_data(conn, user):
+    try:
+        cursor= conn.cursor()
+        query = """
+        INSERT INTO api_users(name,email,city)
+        VALUES(%s,%s,%s)
+        """
+        for u in user:
+            cursor.execute(query,(
+                u['name'],
+                u['email'],
+                u['address']['city']
+            )
+                           )
+        conn.commit()
+        print("API data inserted successfully")
+
+    except pymysql.MySQLError as e:
+        print("insert api data error:", e)
+
+
+    finally:
         cursor.close()        
 
 conn = get_mysql_connection()
 api_data = fetch_api_data()
 if conn:
     create_table(conn)
+    insert_api_data(conn, api_data)
+
 
 conn.close()
 print("MySQL connection closed")
