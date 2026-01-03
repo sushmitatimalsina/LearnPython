b23293e25df7976c1604f6e3cda5e8865a4db936
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2026-01-03 22:29:01 +0545

    feat: Add user list and print functionality to exam.py

--- exam.py
+++ exam.py
@@ -2,3 +2,11 @@ def clean_email(email):
     if email is None:
         return "noemail@unknown.com"
     return email.lower()
+
+users = [
+    {"id": 1, "name": "Ram"},
+    {"id": 2, "name": "Sita"}
+]
+
+for user in users:
+    print(user["name"])
