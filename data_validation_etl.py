f42b948097e3cf43bc301095f556bac5c1cf02ce
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2026-01-04 21:42:32 +0545

    feat: Enhance user validation by adding user list processing and output of valid users

--- data_validation_etl.py
+++ data_validation_etl.py
@@ -4,3 +4,17 @@ def validate_user(user):
     if not user.get("email"):
         return False
     return True
+
+
+users = [
+    {"id": 1, "email": "ram@gmail.com"},
+    {"id": 2, "email": None}
+]
+
+valid_users = []
+
+for user in users:
+    if validate_user(user):
+        valid_users.append(user)
+
+print(valid_users)
