5f5e25770f03f1c387cd2a8c831820a64b976fb7
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2026-01-06 22:44:02 +0545

    feat: remove missing values and created a new columns with the total prices  in sales

--- etl_sales_pipelines.py
+++ etl_sales_pipelines.py
@@ -4,4 +4,7 @@ def extract_data():
     df = pd.read_csv('raw_sales_data.csv')
     return df
 
-
+def transform_data(df):
+    df.dropna(inplace=True)
+    df['total_price'] = df['quantity']*df['unit_price']
+    return df
