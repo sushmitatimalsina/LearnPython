451bc0cf4012d835f08232ab22ed6c023a1a260e
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2026-01-06 23:01:28 +0545

    feat: Fill missing quantity with text (not available)  in quantity

--- etl_sales_pipelines.py
+++ etl_sales_pipelines.py
@@ -5,10 +5,23 @@ def extract_data():
     return df
 
 def transform_data(df):
+    df['quantity'] = df['quantity'].fillna("Not Available")
+    
     df.dropna(inplace=True)
-    df['total_price'] = df['quantity']*df['unit_price']
+    df['total_price'] = df['quantity']*df['price']
     return df
 
 def load_data(df):
     df.to_csv("cleaned_sales_data.csv", index=False)
-    
+
+
+def main():
+    raw_data = extract_data()
+    clean_data = transform_data(raw_data)
+    load_data(clean_data)
+
+
+
+if __name__ == "__main__":
+    main()
+
