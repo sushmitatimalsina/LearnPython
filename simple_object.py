a9a896f4589c4d56e9a8cb2ec094d32b12011395
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2026-01-11 22:55:08 +0545

    feat: ceated  a job object  and print the job title

--- simple_object.py
+++ simple_object.py
@@ -1,16 +1,24 @@
-student = {
-    "name": "Sushmita Timalsina",
-    "age": 22,
-    "courses": ["Math", "Science", "History"]
+# student = {
+#     "name": "Sushmita Timalsina",
+#     "age": 22,
+#     "courses": ["Math", "Science", "History"]
     
+# }
+
+# print(student)
+# print(student["name"])
+# print(student["courses"][1])
+# student["age"] = 23
+# student["email"] = "ajjashdahsj@gmail.com"
+# print(student)
+# del student["age"]
+# print(student)
+
+
+job = {
+    "job_id": 101,
+    "title": "Data Engineer",
+    "location": "Kathmandu",
+    "salary": 50000
 }
-
-print(student)
-print(student["name"])
-print(student["courses"][1])
-student["age"] = 23
-student["email"] = "ajjashdahsj@gmail.com"
-print(student)
-del student["age"]
-print(student)
-
+print(job["title"])
