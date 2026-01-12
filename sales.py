0414715104cf007b3b4b490eb67292de0d942746
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2026-01-12 23:04:57 +0545

    feat: created the sales and print the sales data

---
+++ sales.py
@@ -0,0 +1,7 @@
+sales = [
+    {"product": "Pen", "quantity": 10, "price": 5},
+    {"product": "Notebook", "quantity": 5, "price": 50}
+]
+
+for items in sales:
+    print(items["product"], items["quantity"])
