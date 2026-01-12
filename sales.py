5bfdd47d54e4e8430bee830da3ec9fb2988f0ae9
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2026-01-12 23:11:04 +0545

    feat: handle the missing values in the sales

--- sales.py
+++ sales.py
@@ -10,5 +10,10 @@ for items in sales:
 print("----- After Adding Total Price -----")
 for items in sales:
     print(items["product"], "Total Price:", items["total_price"])
-    
 
+for i in sales:
+    if i["price"]== None:
+        i["price"]= "Not Available"
+    print(i["product"], "-", i["price"])
+    
+        
