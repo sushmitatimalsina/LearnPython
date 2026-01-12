e4eca460cab88c5a836600a3e20623df4971e53f
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2026-01-12 23:15:50 +0545

    feat: Added  raw sales source data for ETL pipeline

---
+++ etl_mini_project/data/raw_data.py
@@ -0,0 +1,5 @@
+sales_data = [
+    {"order_id": 1, "product": "Pen", "quantity": 10, "price": 5},
+    {"order_id": 2, "product": "Notebook", "quantity": 5, "price": 50},
+    {"order_id": 3, "product": "Pencil", "quantity": 20, "price": None}
+]
