e403b58ea1b9b6f77a1481dff1c92efb38b3e389
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2026-03-01 22:17:52 +0545

    feat: implement main pipeline to extract, transform, and load sales data

---
+++ sales-data-pipeline/src/main.py
@@ -0,0 +1,15 @@
+from extract import extract_data
+from transform import transform_data
+from load import load_data
+
+def run_pipline():
+    raw_path = "sales_data_pipline/data/daily_sales.csv"
+    processed_path = "sales_data_pipline/data/processed/cleaned_sales.csv"
+
+    df = extract_data(raw_path)
+    df = transform_data(df)
+    load_data(df, processed_path)
+
+    print("Pipeline completed successfully!")
+    if __name__ == "__main__":
+        run_pipline()
