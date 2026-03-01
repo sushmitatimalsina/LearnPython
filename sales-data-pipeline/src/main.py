60ebe1b44676d8dc869b8377d8f5f739a62a98e8
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2026-03-01 22:19:20 +0545

    fix: correct file path formatting in main pipeline

--- sales-data-pipeline/src/main.py
+++ sales-data-pipeline/src/main.py
@@ -3,8 +3,8 @@ from transform import transform_data
 from load import load_data
 
 def run_pipline():
-    raw_path = "sales_data_pipline/data/daily_sales.csv"
-    processed_path = "sales_data_pipline/data/processed/cleaned_sales.csv"
+    raw_path = "sales-data-pipline/data/daily_sales.csv"
+    processed_path = "sales-data-pipline/data/processed/cleaned_sales.csv"
 
     df = extract_data(raw_path)
     df = transform_data(df)
