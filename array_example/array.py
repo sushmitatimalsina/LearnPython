22a188293cc22d495766218f62d6a1a02f3ba839
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2026-03-14 21:25:08 +0545

    fix: correct formatting of print statements for third sales value in array.py

--- array_example/array.py
+++ array_example/array.py
@@ -2,4 +2,8 @@ sales = [1200, 1500, 900, 2000, 1700]
 
 print(sales)
 print(sales[0])   # first value
-print(sales[2])   # third value
+print(sales[2])  
+ # third value
+
+for s in sales:
+    print(s)
