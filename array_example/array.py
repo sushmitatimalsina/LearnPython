50ff49a3449a3ce543e265ded6b68e2ffce8f82f
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2026-03-14 21:28:18 +0545

    fix: comment out sales-related code and ensure consistent formatting in array.py

--- array_example/array.py
+++ array_example/array.py
@@ -1,19 +1,35 @@
-sales = [1200, 1500, 900, 2000, 1700]
+# sales = [1200, 1500, 900, 2000, 1700]
 
-print(sales)
-print(sales[0])   # first value
-print(sales[2])  
- # third value
+# print(sales)
+# print(sales[0])   # first value
+# print(sales[2])  
+#  # third value
 
-for s in sales:
-    print(s)
+# for s in sales:
+#     print(s)
 
-def calculate_total(sales):
-    total = sum(sales)
-    return total
+# def calculate_total(sales):
+#     total = sum(sales)
+#     return total
 
-sales = [1200, 1500, 900, 2000]
+# sales = [1200, 1500, 900, 2000]
 
-result = calculate_total(sales)
+# result = calculate_total(sales)
 
-print("Total Sales:", result)
+# print("Total Sales:", result)
+
+def calculate_revenue(prices, quantities):
+    revenues = []
+
+    for i in range(len(prices)):
+        revenues.append(prices[i] * quantities[i])
+
+    return revenues
+
+
+prices = [100, 200, 150]
+quantities = [2, 3, 4]
+
+result = calculate_revenue(prices, quantities)
+
+print("Revenue per product:", result)
