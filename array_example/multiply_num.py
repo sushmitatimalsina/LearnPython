37399e8a4020afae4213204e16b0032456b8304c
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2026-03-16 22:46:32 +0545

    feat: add function to multiply numbers by two in multiply_num.py

---
+++ array_example/multiply_num.py
@@ -0,0 +1,11 @@
+numbers = [2, 4, 6, 8]
+
+def multiply_by_two(arr):
+    result = []
+    
+    for n in arr:
+        result.append(n * 2)
+        
+    return result
+
+print(multiply_by_two(numbers))
